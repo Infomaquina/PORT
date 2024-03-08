66776820902fb4aef096d6f7adc18f5c9db6b9fd
Brandev <brandeveloper@outlook.br>
2024-03-08 10:29:39 -0300

    🔐 Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,18 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://brandev.netlify.app',
+  integrations: [mdx(), sitemap(), tailwind(), icon()],  
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
---
+++ projeto.cmd
@@ -0,0 +1,4 @@
+start http://localhost/phpmyadmin/
+start https://localhost:3000
+start PORT.code-workspace
+start cmd /k "npm run dev"
