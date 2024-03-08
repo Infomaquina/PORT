272840f00e41aa691836c18bc0ae5ab88a718264
Brandev <brandeveloper@outlook.br>
2024-03-08 11:38:30 -0300

    🚑 Sem ssl

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://brandev.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind(), icon()],  
-  vite: {
-    plugins: [basicSsl()],
-    server: {
-      https: true,
-    },
-  },
+  integrations: [mdx(), sitemap(), tailwind(), icon()]
 });
