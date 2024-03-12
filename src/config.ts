e1fd84332c377b436d4bcf4361f3314f9a35683c
Brandev <brandeveloper@outlook.br>
2024-03-12 16:53:05 -0300

    🚀 Portfolio

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Brendev Tech | Personal Portfolio';
+export const SITE_TITLE = 'Brendev Tech';
 export const SITE_DESCRIPTION = 'This Personal Portfolio Website built with Astro and TailwindCSS.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
